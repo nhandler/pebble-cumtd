bbb752504553f940dfd2b4f0789031e1d5d01a4f
Nathan Handler <nathan.handler@gmail.com>
2014-02-26 12:49:41 -0600

    Initial Commit

---
+++ src/cumtd.c
@@ -0,0 +1,327 @@
+#include <pebble.h>
+
+#define NUM_MENU_ITEMS 5
+#define DEP_NUM_MENU_ITEMS 5
+
+static Window *window;
+static Window *dep_window;
+
+static MenuLayer *menu_layer;
+static MenuLayer *dep_menu_layer;
+
+static char *stops[NUM_MENU_ITEMS];
+static char *codes[NUM_MENU_ITEMS];
+static int menu_size = 0;
+
+static char *headsigns[DEP_NUM_MENU_ITEMS];
+static char *times[DEP_NUM_MENU_ITEMS];
+static char *dep_code = "Departures";
+static int dep_menu_size = 0;
+
+static const bool animated = true;
+
+enum {
+  stop = 0,
+  code = 1
+};
+
+enum {
+  headsign = 2,
+  esttime = 3
+};
+
+static void update_code(char *new_code) {
+  if (dep_code) {
+    free(dep_code);
+  }
+  dep_code = malloc(sizeof(char)*(strlen(new_code)+1+12));
+  strcpy(dep_code, "Departures: ");
+  strcat(dep_code, new_code);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "New Dep Code: %s", dep_code);
+}
+
+static void append_menu(char *stop, char *code) {
+  if (menu_size == NUM_MENU_ITEMS) {
+    return;
+  }
+  stops[menu_size] = malloc(sizeof(char)*(strlen(stop)+1));
+  strcpy(stops[menu_size], stop);
+  codes[menu_size] = malloc(sizeof(char)*(strlen(code)+1));
+  strcpy(codes[menu_size], code);
+  menu_size += 1;
+
+  menu_layer_reload_data(menu_layer);
+}
+
+static void dep_append_menu(char *headsign, char *minutes) {
+  if (dep_menu_size == DEP_NUM_MENU_ITEMS) {
+    return;
+  }
+  headsigns[dep_menu_size] = malloc(sizeof(char)*(strlen(headsign)+1));
+  strcpy(headsigns[dep_menu_size], headsign);
+  times[dep_menu_size] = malloc(sizeof(char)*(strlen(minutes)+1));
+  strcpy(times[dep_menu_size], minutes);
+  dep_menu_size += 1;
+
+  menu_layer_reload_data(dep_menu_layer);
+}
+
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return 1;
+}
+
+static uint16_t dep_menu_get_num_sections_callback(MenuLayer *dep_menu_layer, void *data) {
+  return 1;
+}
+
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  if (section_index == 0) {
+    return menu_size;
+  }
+  else {
+    return 0;
+  }
+}
+
+static uint16_t dep_menu_get_num_rows_callback(MenuLayer *dep_menu_layer, uint16_t section_index, void *data) {
+  if (section_index == 0) {
+    return dep_menu_size;
+  }
+  else {
+    return 0;
+  }
+}
+
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
+
+static int16_t dep_menu_get_header_height_callback(MenuLayer *dep_menu_layer, uint16_t section_index, void *data) {
+  return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
+
+static void menu_draw_header_callback(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  if (section_index == 0) {
+    menu_cell_basic_header_draw(ctx, cell_layer, "Nearby Stops");
+  }
+}
+
+static void dep_menu_draw_header_callback(GContext *ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  if (section_index == 0) {
+    menu_cell_basic_header_draw(ctx, cell_layer, dep_code);
+  }
+}
+
+static void menu_draw_row_callback(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  if (cell_index->section == 0) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Drawing Row: %s, %s", stops[cell_index->row], codes[cell_index->row]);
+    menu_cell_basic_draw(ctx, cell_layer, stops[cell_index->row], codes[cell_index->row], NULL);
+  }
+}
+
+static void dep_menu_draw_row_callback(GContext *ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  if (cell_index->section == 0) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Drawing Row: %s, %s", headsigns[cell_index->row], times[cell_index->row]);
+    menu_cell_basic_draw(ctx, cell_layer, headsigns[cell_index->row], times[cell_index->row], NULL);
+  }
+}
+
+void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+  if (cell_index->section == 0) {
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    Tuplet value = TupletCString(code, codes[cell_index->row]);
+    dict_write_tuplet(iter, &value);
+    app_message_outbox_send();
+  }
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sending Subtitle: %s", codes[cell_index->row]);
+  window_stack_push(dep_window, animated);
+}
+
+void dep_menu_select_callback(MenuLayer *dep_menu_layer, MenuIndex *cell_index, void *data) {
+/*  if (cell_index->section == 0) {
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    Tuplet value = TupletCString(code, codes[cell_index->row]);
+    dict_write_tuplet(iter, &value);
+    app_message_outbox_send();
+  }*/
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Sending Subtitle: %s", times[cell_index->row]);
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+  menu_layer = menu_layer_create(bounds);
+
+  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
+    .get_num_sections = menu_get_num_sections_callback,
+    .get_num_rows = menu_get_num_rows_callback,
+    .get_header_height = menu_get_header_height_callback,
+    .draw_header = menu_draw_header_callback,
+    .draw_row = menu_draw_row_callback,
+    .select_click = menu_select_callback,
+  });
+
+  menu_layer_set_click_config_onto_window(menu_layer, window);
+
+  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
+}
+
+static void dep_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+  dep_menu_layer = menu_layer_create(bounds);
+
+  menu_layer_set_callbacks(dep_menu_layer, NULL, (MenuLayerCallbacks){
+    .get_num_sections = dep_menu_get_num_sections_callback,
+    .get_num_rows = dep_menu_get_num_rows_callback,
+    .get_header_height = dep_menu_get_header_height_callback,
+    .draw_header = dep_menu_draw_header_callback,
+    .draw_row = dep_menu_draw_row_callback,
+    .select_click = dep_menu_select_callback,
+  });
+
+  menu_layer_set_click_config_onto_window(dep_menu_layer, window);
+
+  layer_add_child(window_layer, menu_layer_get_layer(dep_menu_layer));
+}
+
+static void window_unload(Window *window) {
+  int i;
+  for (i = 0 ; i < NUM_MENU_ITEMS ; i += 1) {
+    free(stops[i]);
+    free(codes[i]);
+  }
+  menu_size = 0;
+  menu_layer_destroy(menu_layer);
+}
+
+static void dep_window_unload(Window *window) {
+  int i;
+  for (i = 0 ; i < DEP_NUM_MENU_ITEMS ; i += 1) {
+    free(headsigns[i]);
+    free(times[i]);
+  }
+  dep_menu_size = 0;
+  free(dep_code);
+  menu_layer_destroy(dep_menu_layer);
+}
+
+static void deinit(void) {
+  window_destroy(dep_window);
+  window_destroy(window);
+}
+
+void out_sent_handler(DictionaryIterator *sent, void *context) {
+  // outgoing message was delivered
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Outgoing Message Delivered");
+}
+
+
+void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+  // outgoing message failed
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Outgoing Message Failed");
+}
+
+void in_received_handler(DictionaryIterator *received, void *context) {
+  // incoming message received
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message");
+
+  // Check for fields you expect to receive
+  Tuple *stop_tuple = dict_find(received, stop);
+  if(!stop_tuple) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "NO STOP");
+  }
+  else {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "HAVE STOP");
+  }
+  Tuple *code_tuple = dict_find(received, code);
+  if(!code_tuple) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "NO CODE");
+  }
+  else {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "HAVE CODE");
+  }
+
+  if (stop_tuple && code_tuple) {
+    char *stopString = stop_tuple->value->cstring;
+    char *codeString = code_tuple->value->cstring;
+  
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Stop: %s", stopString);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Code: %s", codeString);
+
+    append_menu(stopString, codeString);
+    return;
+  }
+  if (code_tuple) {
+    char *codeString = code_tuple->value->cstring;
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Code: %s", codeString);
+
+    update_code(codeString);
+  }
+  Tuple *hs_tuple = dict_find(received, headsign);
+  if(!hs_tuple) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "NO HS");
+  }
+  else {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "HAVE HS");
+  }
+  Tuple *time_tuple = dict_find(received, esttime);
+  if(!time_tuple) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "NO TIME");
+  }
+  else {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "HAVE TIME");
+  }
+  if (hs_tuple && time_tuple) {
+    char *hsString = hs_tuple->value->cstring;
+    char *timeString = time_tuple->value->cstring;
+
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "HS: %s", hsString);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "TIME: %s", timeString);
+
+    dep_append_menu(hsString, timeString);
+    return;
+  }
+}
+
+
+void in_dropped_handler(AppMessageResult reason, void *context) {
+  // incoming message dropped
+}
+
+static void init(void) {
+  app_message_register_inbox_received(in_received_handler);
+  app_message_register_inbox_dropped(in_dropped_handler);
+  app_message_register_outbox_sent(out_sent_handler);
+  app_message_register_outbox_failed(out_failed_handler);
+
+  const uint32_t inbound_size = 64;
+  const uint32_t outbound_size = 64;
+  app_message_open(inbound_size, outbound_size);
+
+  window = window_create();
+//  window_set_click_config_provider(window, click_config_provider);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  dep_window = window_create();
+//  window_set_click_config_provider(dep_window, click_config_provider);
+  window_set_window_handlers(dep_window, (WindowHandlers) {
+    .load = dep_window_load,
+    .unload = dep_window_unload,
+  });
+  window_stack_push(window, animated);
+}
+
+int main(void) {
+  init();
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+
+  app_event_loop();
+  deinit();
+}
---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,165 @@
+var messageSendQueue = [];
+var sent = 0;
+var received = 0;
+function sendNextMessage() {
+  if (messageSendQueue.length == 0) {
+    return;
+  }
+  var transactionId = Pebble.sendAppMessage( messageSendQueue.shift(), messageSendSuccess,messageSendFail );
+  sent++;
+} 
+
+function addMessage(dict) {
+  messageSendQueue.push(dict);
+  if (sent == received) {
+    sendNextMessage();
+  }
+}
+  
+function messageReceived(e) {
+  console.log("JavaScript Received message: " + e.payload.code);
+  getDepartures(e.payload.code);
+}
+
+function messageSendSuccess(e) {
+  console.log("Successfully delivered message with transactionId=" + e.data.transactionId);
+  received++;
+  sendNextMessage();
+}
+
+function messageSendFail(e) {
+  console.log("Unable to deliver message with transactionId=" + e.data.transactionId + " Error is: " + e.error.message);
+  received++;
+  sendNextMessage();
+}
+
+function gotLocation(position) {
+  console.log("Latitude: " + position.coords.latitude);
+  console.log("Longitude: " + position.coords.longitude);
+  getStops(position.coords.latitude, position.coords.longitude);
+  //var coords = position.coords.latitude + "," + position.coords.longitude;
+  //var transactionId = Pebble.sendAppMessage( { "firstKey": 42, "name": coords }, messageSendSuccess,messageSendFail );
+}
+
+function getLocation() {
+  if ("geolocation" in navigator) {
+    /* geolocation is available */
+    console.log("Geolocation is available");
+    navigator.geolocation.getCurrentPosition(gotLocation);
+  }
+  else {
+    /* geolocation is NOT available */
+    console.log("Geolocation is NOT available");
+    //var transactionId = Pebble.sendAppMessage( { "firstKey": 42, "name": "Nathan" }, messageSendSuccess,messageSendFail );
+  }
+}
+
+function getStops(lat,lon) {
+  console.log("Getting Stops");
+  var baseUrl = "https://developer.cumtd.com/api/v2.2/json/GetStopsByLatLon?";
+  var key = "d65939a6432b46b38a65b53a998ed15f";
+  var count = 5;
+  var url = baseUrl + "key=" + key + "&lat=" + lat + "&lon=" + lon + "&count=" + count;
+  var req = new XMLHttpRequest();
+  console.log("URL: " + url);
+  req.open('GET', url, true);
+  req.onload = function(e) {
+    console.log("Request onload Function");
+    if (req.readyState == 4 && req.status == 200) {
+      if(req.status == 200) {
+        var response = JSON.parse(req.responseText);
+        var stop1= response.stops[0].stop_name;
+        var stop2= response.stops[1].stop_name;
+        var stop3= response.stops[2].stop_name;
+        var stop4= response.stops[3].stop_name;
+        var stop5= response.stops[4].stop_name;
+        var code1= response.stops[0].stop_id;
+        var code2= response.stops[1].stop_id;
+        var code3= response.stops[2].stop_id;
+        var code4= response.stops[3].stop_id;
+        var code5= response.stops[4].stop_id;
+        console.log("Stop1: " + stop1);
+        console.log("Stop2: " + stop2);
+        console.log("Stop3: " + stop3);
+        console.log("Stop4: " + stop4);
+        console.log("Stop5: " + stop5);
+        console.log("Code1: " + code1);
+        console.log("Code2: " + code2);
+        console.log("Code3: " + code3);
+        console.log("Code4: " + code4);
+        console.log("Code5: " + code5);
+        addMessage({"stop":stop1,"code":code1});
+        addMessage({"stop":stop2,"code":code2});
+        addMessage({"stop":stop3,"code":code3});
+        addMessage({"stop":stop4,"code":code4});
+        addMessage({"stop":stop5,"code":code5});
+      }
+      else {
+        console.log("Error Getting Stops");
+      }
+    }
+    else {
+      console.log("Unknown Error Getting Stops: " + req.status);
+    }
+  }
+  req.send(null);
+}
+
+function getDepartures(stop) {
+  console.log("Getting Stops");
+  var baseUrl = "https://developer.cumtd.com/api/v2.2/json/GetDeparturesByStop?";
+  var key = "d65939a6432b46b38a65b53a998ed15f";
+  var count = 5;
+  var url = baseUrl + "key=" + key + "&stop_id=" + stop + "&count=" + count;
+  var req = new XMLHttpRequest();
+  console.log("URL: " + url);
+  req.open('GET', url, true);
+  req.onload = function(e) {
+    console.log("Request onload Function");
+    if (req.readyState == 4 && req.status == 200) {
+      if(req.status == 200) {
+        var response = JSON.parse(req.responseText);
+        var hs1 = response.departures[0].headsign;
+        var time1 = response.departures[0].expected_mins.toString() + " Minutes";
+        var hs2 = response.departures[1].headsign;
+        var time2 = response.departures[1].expected_mins.toString() + " Minutes";
+        var hs3 = response.departures[2].headsign;
+        var time3 = response.departures[2].expected_mins.toString() + " Minutes";
+        var hs4 = response.departures[3].headsign;
+        var time4 = response.departures[3].expected_mins.toString() + " Minutes";
+        var hs5 = response.departures[4].headsign;
+        var time5 = response.departures[4].expected_mins.toString() + " Minutes";
+        console.log("HS1: " + hs1);
+        console.log("HS2: " + hs2);
+        console.log("HS3: " + hs3);
+        console.log("HS4: " + hs4);
+        console.log("HS5: " + hs5);
+        console.log("TIME1: " + time1);
+        console.log("TIME2: " + time2);
+        console.log("TIME3: " + time3);
+        console.log("TIME4: " + time4);
+        console.log("TIME5: " + time5);
+        addMessage({"code":stop,"headsign":hs1,"esttime":time1});
+        addMessage({"code":stop,"headsign":hs2,"esttime":time2});
+        addMessage({"code":stop,"headsign":hs3,"esttime":time3});
+        addMessage({"code":stop,"headsign":hs4,"esttime":time4});
+        addMessage({"code":stop,"headsign":hs5,"esttime":time5});
+      }
+      else {
+        console.log("Error Getting Departures");
+      }
+    }
+    else {
+      console.log("Unknown Error Getting Departures: " + req.status);
+    }
+  }
+  req.send(null);
+}
+Pebble.addEventListener("ready",
+    function(e) {
+        console.log("Hello world! - Sent from your javascript application.");
+        console.log("Pebble Account Token: " + Pebble.getAccountToken());
+        Pebble.addEventListener("appmessage", messageReceived);
+        getLocation();
+    }
+);
